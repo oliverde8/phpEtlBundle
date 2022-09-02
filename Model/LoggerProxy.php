a0d60214eb45bc249e21d626fb86690de221ea34
Mehdi Chaouch <mehdichaouch@users.noreply.github.com>
2022-09-02 19:39:05 +0200

    Update logger proxy method signature

--- Model/LoggerProxy.php
+++ Model/LoggerProxy.php
@@ -23,47 +23,47 @@ class LoggerProxy implements LoggerInterface
         $this->executionContext = $executionContext;
     }
 
-    public function emergency($message, array $context = array())
+    public function emergency($message, array $context = array()): void
     {
         $this->logger->emergency($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function alert($message, array $context = array())
+    public function alert($message, array $context = array()): void
     {
         $this->logger->alert($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function critical($message, array $context = array())
+    public function critical($message, array $context = array()): void
     {
         $this->logger->critical($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function error($message, array $context = array())
+    public function error($message, array $context = array()): void
     {
         $this->logger->error($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function warning($message, array $context = array())
+    public function warning($message, array $context = array()): void
     {
         $this->logger->warning($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function notice($message, array $context = array())
+    public function notice($message, array $context = array()): void
     {
         $this->logger->notice($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function info($message, array $context = array())
+    public function info($message, array $context = array()): void
     {
         $this->logger->info($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function debug($message, array $context = array())
+    public function debug($message, array $context = array()): void
     {
         $this->logger->debug($message, $this->executionContext->getLoggerContext($context));
     }
 
-    public function log($level, $message, array $context = array())
+    public function log($level, $message, array $context = array()): void
     {
         $this->logger->log($level, $message, $this->executionContext->getLoggerContext($context));
     }
